973e52a25ec7b5cf9104d6a227a24c32cc219541
Алекс <Alexandr_enid@mail.ru>
2023-02-02 22:10:18 +0500

    Добавьте файлы проекта.

---
+++ Homework_in_head/Source.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+class A {
+public:
+	virtual ~A() {}	// добавляем виртуальный деструктор от утечки памяти (вроде так)
+	virtual void foo(std::vector<int>& vec) = 0;
+};
+
+class B : public A {
+public:
+	void foo(std::vector<int>& vec) override {
+		std::cout << "foo function" << std::endl;
+		vec.erase(vec.begin());
+	}
+};
+
+int main() {
+
+	A* b = new B();
+	std::vector<int> v = { 1,2,3 }; // остается два элемента 2 и 3, потому что в функции единичка удаляется методом erase (первый элемент - begin)
+	b->foo(v);
+	delete b;	// объект нужно удалить, потому как это указатель на класс А, чтобы не засорял память
+}// Что можно сделать с объявлением foo, чтобы v имело другое количество элементов ?// Наверное в параметрах функции убрать амперсант, тогда вектор будет копироваться и удалять копию первого элемента
